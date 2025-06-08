1a2a16fd0ea5842536e464b866ea6559019a3f27
NR1000-coder <nrminseyfullayeva@gmail.com>
2025-06-08 10:34:17 +0000

    commitSecond

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     $(document).on("keypress", function(e) {
         if (e.which == 13) {
-            let url `https://apidemo-9qn8.vercel.app/api/news`
+            let url = `https://apidemo-9qn8.vercel.app/api/news`;
             async function mynews() {
                 let responce = await fetch(url)
                 let data = await responce.json()
